9471b333f7329fb552287b9fafb4247015f667b0
nijot <nijot@leanhorus.com>
2019-03-13 09:43:49 +0000

    Update to docker

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,19 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) throws Throwable {
-        SpringApplication.run(Application.class, args);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
